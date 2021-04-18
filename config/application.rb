f78506fc0543dd8a44ba6cecdcea606e8ed83290
Dmitry Zaguta <zdvbind@gmail.com>
2021-04-18 21:00:27 +0500

    Change timezone to Ekb, default_locale to ru

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module TestGuru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.time_zone = 'Ekaterinburg'
+    config.i18n.default_locale = :ru
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
