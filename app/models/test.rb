b00edf88557b4a7298dbb5fc3cc1962f8fdb263e
Dmitry Zaguta <zdvbind@gmail.com>
2021-04-26 21:55:07 +0500

    add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  has_many :questions, dependent: :destroy
+  has_many :test_results, dependent: :destroy
+  has_many :users, through: :test_results, dependent: :destroy
+
   def self.titles_by_category(cat)
     joins('JOIN categories ON category_id = categories.id')
       .where(categories: { title: cat })
