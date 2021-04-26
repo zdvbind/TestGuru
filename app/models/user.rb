8e15c1562e5116954ff21e7062441888899dd94a
Dmitry Zaguta <zdvbind@gmail.com>
2021-04-26 22:26:14 +0500

    change instance method tests_by_level in model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :tests, through: :test_results, dependent: :destroy
   has_many :author_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
   def tests_by_level(level)
-    Test.joins('JOIN test_results ON tests.id = test_results.test_id')
-        .where(tests: { level: level }, test_results: { user_id: id })
+    tests.where(level: level)
   end
 end
