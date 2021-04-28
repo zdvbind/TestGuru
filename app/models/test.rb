491fc9a628d2b26a0cc8817a67717fc518c0d213
Dmitry Zaguta <zdvbind@gmail.com>
2021-04-28 13:04:23 +0500

    fix model Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,7 +4,7 @@ class Test < ApplicationRecord
 
   has_many :questions, dependent: :destroy
   has_many :test_results, dependent: :destroy
-  has_many :users, through: :test_results, dependent: :destroy
+  has_many :users, through: :test_results
 
   def self.titles_by_category(cat)
     joins(:category)
