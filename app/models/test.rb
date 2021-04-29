6fdd1e94cd0ef8cb179d4ce332106af93fe31953
Dmitry Zaguta <zdvbind@gmail.com>
2021-04-29 15:34:45 +0500

    channge scope

--- app/models/test.rb
+++ app/models/test.rb
@@ -15,9 +15,9 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
-  scope :by_category, ->(cat) { joins(:category).where(categories: { title: cat }).order(title: :desc) }
+  scope :by_category, ->(cat) { joins(:category).where(categories: { title: cat }) }
 
   def self.titles_by_category(cat)
-    by_category(cat).pluck(:title)
+    by_category(cat).order(title: :desc).pluck(:title)
   end
 end
