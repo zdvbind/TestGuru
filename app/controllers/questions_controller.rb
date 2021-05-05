f0a2a7f220ffea6f494a5f80f016336c53599309
Dmitry Zaguta <zdvbind@gmail.com>
2021-05-05 16:14:58 +0500

    add method index to question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,13 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index]
+
+  def index
+    render plain: @test.questions.pluck(:body).join("\n")
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
 end
