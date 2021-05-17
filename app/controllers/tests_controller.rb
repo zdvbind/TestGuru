468c8d8078ea8409229984e692b22f893a1a7790
Dmitry Zaguta <zdvbind@gmail.com>
2021-05-17 13:29:48 +0500

    add find_test before_action to tests_controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,19 +1,16 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show edit update destroy]
   def index
     @tests = Test.all
   end
 
-  def show
-    @test = Test.find(params[:id])
-  end
+  def show; end
 
   def new
     @test = Test.new
   end
 
-  def edit
-    @test = Test.find(params[:id])
-  end
+  def edit; end
 
   def create
     @test = Test.new(test_params)
@@ -26,8 +23,6 @@ class TestsController < ApplicationController
   end
 
   def update
-    @test = Test.find(params[:id])
-
     if @test.update(test_params)
       redirect_to @test
     else
@@ -36,8 +31,6 @@ class TestsController < ApplicationController
   end
 
   def destroy
-    @test = Test.find(params[:id])
-
     @test.destroy
     redirect_to tests_path
   end
@@ -46,5 +39,9 @@ class TestsController < ApplicationController
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
   end
 end
