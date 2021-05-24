c5cd560bda95c6656484897725307f4f52821450
Dmitry Zaguta <zdvbind@gmail.com>
2021-05-24 16:22:51 +0500

    add  action start in test_controller, add method test_passage to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
@@ -7,5 +8,9 @@ class User < ApplicationRecord
 
   def tests_by_level(level)
     tests.where(level: level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
