59dcffba302001376468ea39c6dd1f52bd1574e3
Dmitry Zaguta <zdvbind@gmail.com>
2021-05-24 17:23:33 +0500

    fix buf in migrate, add option to association current_question

--- db/migrate/20210524083534_create_test_passages.rb
+++ db/migrate/20210524083534_create_test_passages.rb
@@ -3,7 +3,7 @@ class CreateTestPassages < ActiveRecord::Migration[6.1]
     create_table :test_passages do |t|
       t.references :user, null: false, foreign_key: true
       t.references :test, null: false, foreign_key: true
-      t.references :current_question, null: false, foreign_key: true
+      t.references :current_question
       t.integer :correct_questions, default: 0
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_05_24_085038) do
   create_table "test_passages", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "test_id", null: false
-    t.integer "current_question_id", null: false
+    t.integer "current_question_id"
     t.integer "correct_questions", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -68,7 +68,6 @@ ActiveRecord::Schema.define(version: 2021_05_24_085038) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
-  add_foreign_key "test_passages", "current_questions"
   add_foreign_key "test_passages", "tests"
   add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
