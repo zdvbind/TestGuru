78010efd2dbd040acb941524d1c4c1a8d68b840a
Dmitry Zaguta <zdvbind@gmail.com>
2021-05-26 16:28:47 +0500

    add column password_digest to table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_085038) do
+ActiveRecord::Schema.define(version: 2021_05_26_112701) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_05_24_085038) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
