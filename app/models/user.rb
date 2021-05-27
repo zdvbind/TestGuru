05ecd1b2eba02ba9eaf03759534652227c4efe2c
Dmitry Zaguta <zdvbind@gmail.com>
2021-05-27 22:04:46 +0500

    add sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
-  include Auth
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
+
+  has_secure_password
 
   def tests_by_level(level)
     tests.where(level: level)
