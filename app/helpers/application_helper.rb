a23a96c4fa8f6148f65e083752121c13bea36762
Dmitry Zaguta <zdvbind@gmail.com>
2021-05-28 18:53:44 +0500

    add helper for flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to repo, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def flash_message(message, type)
+    content_tag :p, message, class: "flash #{type}"
+  end
 end
