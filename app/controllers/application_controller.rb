37a44505ed33cddcbfa7d82b3f841127b2d4f86b
Dmitry Zaguta <zdvbind@gmail.com>
2021-05-28 14:26:56 +0500

    Add another flash, add text to layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
   private
 
   def authenticate_user!
     unless current_user
-      redirect_to login_path
+      redirect_to login_path, alert: 'Are you a Guru? Verify your email and password please'
     end
   end
 
