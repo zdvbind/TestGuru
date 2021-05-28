9ab5a9c04660fd7d0cf084749244e35f09d75ee9
Dmitry Zaguta <zdvbind@gmail.com>
2021-05-28 18:02:47 +0500

    add validate presence and uniqueness email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates :name, presence: true
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+
   def tests_by_level(level)
     tests.where(level: level)
   end
