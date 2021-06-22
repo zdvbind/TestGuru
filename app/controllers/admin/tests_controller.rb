b615716d859b603c164b9f20920ddf2fac830e09
Dmitry Zaguta <zdvbind@gmail.com>
2021-06-23 00:04:20 +0500

    small fix (delete comments etc)

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,6 +1,6 @@
 class Admin::TestsController < Admin::BaseController
   before_action :set_tests, only: %i[index update_inline]
-  before_action :find_test, only: %i[show edit update destroy start update_inline]
+  before_action :find_test, only: %i[show edit update destroy update_inline]
 
   def index; end
 
