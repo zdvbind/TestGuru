66f68bd1ed8d925dee54772f57d1ec5af3981728
Dmitry Zaguta <zdvbind@gmail.com>
2021-06-27 18:00:53 +0500

    change feedback mailer - It sends lettes to all admins now

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,8 +1,9 @@
 class FeedbackMailer < ApplicationMailer
+  default to: -> { Admin.pluck(:email) }
+
   def feedback(feed, user)
     @user = user
     @feedback = feed[:feedback]
-    @admin = User.where(type: :Admin).order(created_at: :desc).first
-    mail to: @admin.email, subject: feed[:subj]
+    mail subject: feed[:subj]
   end
 end
