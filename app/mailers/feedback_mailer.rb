5fa23bce2364bf58ed7021f418d68bd67df54125
Dmitry Zaguta <zdvbind@gmail.com>
2021-06-27 16:18:50 +0500

    fix method feedback

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -2,7 +2,7 @@ class FeedbackMailer < ApplicationMailer
   def feedback(feed, user)
     @user = user
     @feedback = feed[:feedback]
-    @admin = User.where(type: :Admin).order(created_at: :desc).limit(1).first
+    @admin = User.where(type: :Admin).order(created_at: :desc).first
     mail to: @admin.email, subject: feed[:subj]
   end
 end
