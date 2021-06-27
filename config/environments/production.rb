bdf3309292acc458afa8ccc850c93fe89f4f7317
Dmitry Zaguta <zdvbind@gmail.com>
2021-06-27 16:54:25 +0500

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'test-guru-zdvbind.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'test-guru-zdvbind.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
